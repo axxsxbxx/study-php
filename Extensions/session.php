c18a8571dc7db9d960b3e643bf93ae19828f2923
subin <ansubin2004@gmail.com>
2022-03-23 16:21:36 +0900

    PHP Session

---
+++ Extensions/session.php
@@ -0,0 +1,45 @@
+<?php
+
+// Set session save path
+session_save_path("./sessions");
+
+
+// Start a session
+session_start();
+
+
+// Set session value with key
+$_SESSION["mySession"] = "Hello, world";
+echo $_SESSION["mySession"];
+
+
+// Get session id
+session_id();
+
+
+// Get session name
+session_name();
+
+
+// Run GC(Garbage Collection)
+// 세션의 유효기간이 만료되면 가비지 컬렉션을 통해서 파기시켜줘야한다.
+session_gc();
+
+
+// Set/Get session cookie info
+// 쿠키 수명 지정
+session_set_cookie_params(1440);
+var_dump(session_get_cookie_params());
+
+
+// Remove a Session value
+unset($_SESSION["mySession"]);
+
+
+// Reset Session
+session_unset();
+
+
+// Remove Session file
+// 권장하고 있지는 않다. 가비지 컬렉터 돌리지 못하는 경우에 사용.
+session_destroy();
